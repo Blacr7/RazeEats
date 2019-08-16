7242a20a49c7a9aae49ce08709f693f5202280d3
blacr7 <blacr7@mail.broward.edu>
2019-08-15 20:56:19 -0400

    Working on menu section, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RazeEats
- Restaurant Website
+ Restaurant Website Project using PHP, BOOTSTRAP, SASS.
+
+ #Status
+ Early Development
+ 
+#TO DO
+Add shopping cart functionality, styling for larger resolution, cross browser compatibility
--- menu.php
+++ menu.php
@@ -1,8 +1,58 @@
 <?php require 'scripts/header.php'; ?>
 
-<section class="menu bg-dark">
-    <span>test menu</span>
+<section class="menu bg-dark ">
+    <div class="container">
+        <h2 class="text-white text-center pt-4">Menu</h2>
+        <div class="card-deck flex-sm-column">
+            <div class="card m-2">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1477617722074-45613a51bf6d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Raze Burger</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quaerat quas distinctio? Possimus, aspernatur molestias!</p>
+                    <p class="card-text"></p>
+                    <button type="button" class="btn btn-dark">Add To Cart</button>
+                </div>
+            </div>
+            <div class="card m-2">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1547496502-affa22d38842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1168&q=80" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Raze Salad</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet con sec tetur adipisicing elit.</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet, con sec tetur adipisicing elit. Veritatis, nostrum.</p>
+                    <button type="button" class="btn btn-dark">Add To Cart</button>
+                </div>
+            </div>
+            <div class="card m-2">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1536521642388-441263f88a61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Raze Dog</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ad eaque natus, atque sunt ipsa maxime velit saepe, adipisci ipsam nesciunt vero odit? Quia.</p>
+                    <p class="card-text"></p>
+                    <button type="button" class="btn btn-dark">Add To Cart</button>
+                </div>
+            </div>
+            <div class="card m-2">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Raze of the Day</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque a delectus error asperiores id vitae tenetur distinctio?</p>
+                    <p class="card-text"></p>
+                    <button type="button" class="btn btn-dark">Add To Cart</button>
+                </div>
+            </div>
+            <div class="card m-2">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Raze Special</h4>
+                    <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt earum molestias ex, harum tempora mollitia. Ex, veniam! A temporibus hic quasi. Omnis, expedita consequuntur!</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi porro iusto unde optio.</p>
+                    <button type="button" class="btn btn-dark">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
+
 <?php 
     require 'scripts/carousel.php';
     require 'scripts/orderOnline.php'; 
