1fbc784394ef817d8f5595804408b97d51d4e196
blacr7 <blacr7@mail.broward.edu>
2019-08-21 18:01:58 -0400

    Added orderOnline page working on database code

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,25 +1,25 @@
-<section id="goToContact" class="contact d-flex justify-content-center ">
+<section id="goToContact" class="contact d-flex justify-content-center bg-dark">
     
-    <form class="container align-self-center" method="post" action="https://formspree.io/blacr7@mail.broward.edu">
+    <form class="container align-self-center  text-white" method="post" action="https://formspree.io/blacr7@mail.broward.edu">
         <h2 class="text-center">Contact Us</h2>
         <div class="form-group ">
-            <label for="formName">Name</label>
-            <input type="Text" class="form-control" id="formName" placeholder="Please Enter Your Name">
+            <label for="formName">Name *</label>
+            <input type="Text" class="form-control" id="formName" placeholder="Please Enter Your Name" required>
         </div>
 
         <div class="form-group">
-            <label for="formEmail">Email Address</label>
-            <input type="email" class="form-control" id="formEmail" placeholder="Please Enter Your Email">
+            <label for="formEmail">Email Address *</label>
+            <input type="email" class="form-control" id="formEmail" placeholder="Please Enter Your Email" required>
         </div>
 
         <div class="form-group">
-            <label for="formSubject">Subject</label>
+            <label for="formSubject">Subject </label>
             <input type="text" class="form-control" id="formSubject" placeholder="Please Give A Brief Summary">
         </div>
 
         <div class="form-group">
-            <label for="formTextarea">Message</label>
-            <textarea class="form-control" id="formTextarea" placeholder="Type Your Message here" rows="5"></textarea>
+            <label for="formTextarea">Message *</label>
+            <textarea class="form-control" id="formTextarea" placeholder="Type Your Message here" rows="5" required></textarea>
         </div>
 
         <div class="form-group submit"><input class="form-control" type="submit" value="Submit"></div>
--- scripts/orderOnline.php
+++ scripts/orderOnline.php
@@ -1,5 +1,33 @@
-<section id="orderOnline" class=" bg-dark ">
-    <div class="container d-flex text-white">
-        <span>test ORDER ONLINE</span>
+<section id="orderOnline" class="bg-dark ">
+    <div class="container">
+        <div class="onlineTop">
+            <h4>My Order</h4>
+            <span class="">(0) items</span>
+        </div>
+        
+        <table id="onlineTable" class="table table-dark table-striped table-sm table-responsive-sm">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Total</th>
+                    <th>Remove</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Raze Dog</td>
+                    <td>1</td>
+                    <td>9.98</td>
+                    <td>9.98</td>
+                    <td><button type="button" class="btn btn-danger">Remove</button></td>
+                </tr>
+
+                <tr>
+                    <td><button type="button" class="btn btn-success">Order Now</button></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </section>
